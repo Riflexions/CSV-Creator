798d50fc3dc37cce1878ffb9eb43e51b5928c091
bharathtn <bharathtn12@gmail.com>
2016-08-27 14:51:47 +0530

    Added hide/delete

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    showUploader: false,
     actions: {
         newCSV: function () {
 
@@ -8,15 +9,28 @@ export default Ember.Controller.extend({
         existingCSV: function () {
 
         },
+        clickEx: function () {
+            this.set('showUploader', true);
+
+        },
         loadCsv: function () {
             var fileReader = new FileReader();
-            fileReader.onload = function (e) {
-                console.log(e);
-                var csv = Papa.parse(e.target.result);
-                console.log(csv);
-                console.log(JSON.stringify(csv.data));
+            console.log(fileReader);
+            var fname = this.get('fPath');
+            console.log(fname);
+            if (fname == undefined) {
+                alert("Please input a file");
+            } else {
+                fileReader.onload = function (e) {
+                    console.log(e);
+                    var csv = Papa.parse(e.target.result);
+                    console.log(csv);
+                    console.log(JSON.stringify(csv.data));
+                }   
+                fileReader.readAsText($('#file-up')[0].files[0]);
+
+                this.transitionToRoute('creator.fields');
             }
-            fileReader.readAsText($('#file-up')[0].files[0]);
         }
     }
 });

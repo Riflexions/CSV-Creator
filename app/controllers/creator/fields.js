c2a2a07db0b69395d96279308162763e7daca5d7
bharathtn <bharathtn12@gmail.com>
2016-08-27 15:13:01 +0530

    Cleared JsHint Errors

--- app/controllers/creator/fields.js
+++ app/controllers/creator/fields.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     actions: {
-        addFields: function (name, age) {
+        addFields: function () {
             var self = this;
 
             return self.get('store').findAll('field-list');

5e8130fe9124fbcca509e0e7ff121a81ef6a4e20
bharathtn <bharathtn12@gmail.com>
2016-08-29 12:51:45 +0530

    Removed consolelog

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -41,10 +41,6 @@ export default Ember.Controller.extend({
                 fileReader.onload = function (e) {
                     console.log(e);
                     var csv = Papa.parse(e.target.result);
-                    console.log(csv);
-                    console.log(JSON.stringify(csv.data));
-                    console.log('Total length including fields, data & empty last element : ', csv.data.length);
-
                     utils.deleteOldRecords(self).then(() => {
                         var fields = store.createRecord('field-list', {
                             fields: csv.data.splice(0, 1)[0]

38f0c900b1212a97848174b6808f758f78732448
ASKRajatKaushal <askrajatkaushal@gmail.com>
2016-08-30 22:21:26 +0530

    Final Project

--- app/components/data-entry-table.js
+++ app/components/data-entry-table.js
@@ -2,12 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     actions: {
+        save: function () {
+            this.toggleProperty('isEditable1');
+        },
+        editRecs: function (value, row, col) {
+            this.toggleProperty('isEditable1');
+            this.sendAction('editRecs', value, row, col);
+        },
+        deleteRecs: function (row, col) {
+            this.sendAction('deleteRecs', row, col);
+        },
         setRecordValue(record) {
             receivedRecord = record;
         },
         saveToDS() {
             this.sendAction('addData', receivedRecord);
         }
+
     }
 });
 
--- app/controllers/creator/data-entry.js
+++ app/controllers/creator/data-entry.js
@@ -3,34 +3,61 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         addData(records) {
-            var self = this;
-            var store = self.get('store');
+                var self = this;
+                var store = self.get('store');
 
-            console.log('Record Received: ', records);
+                console.log('Record Received: ', records);
 
-            store.findAll('field-list').then((fields) => {
-                var model = fields.get('firstObject');
-                var fieldsList = model.get('fields');
-                return fieldsList.length;
-            }).then((length) => {
-                store.findAll('csv-model').then((csvModels) => {
-                    var model = csvModels.get('firstObject');
+                store.findAll('field-list').then((fields) => {
+                    var model = fields.get('firstObject');
+                    var fieldsList = model.get('fields');
+                    return fieldsList.length;
+                }).then((length) => {
+                    store.findAll('csv-model').then((csvModels) => {
+                        var model = csvModels.get('firstObject');
+                        var modelsList = model.get('records');
+                        var correctRecord = [];
+
+                        for (var i = 0; i < length; i++) {
+                            if (records[i] != undefined)
+                                correctRecord[i] = records[i];
+                            else
+                                correctRecord[i] = "";
+                        }
+                        modelsList.push(correctRecord);
+                        model.set('records', modelsList);
+                        return model.save();
+                    });
+                }).catch((err) => {
+                    console.log('Following error occurred: ', err);
+                });
+            },
+            deleteRecs: function (row, col) {
+                var self = this;
+                var store = self.get('store');
+                store.findAll('csv-model').then((fields) => {
+                    var store = self.get('store');
+                    var model = fields.get('firstObject');
+                    var fieldsList = model.get('records');
+                    console.log(fieldsList);
+                    console.log("row", row);
+                    console.log("col", col);
+                    fieldsList[row][col] = "";
+                    model.set('fields', fieldsList);
+                    return model.save();
+                });
+            },
+            editRecs: function (value, row, col) {
+                var self = this;
+                var store = self.get('store');
+
+                store.findAll('csv-model').then((models) => {
+                    var model = models.get('firstObject');
                     var modelsList = model.get('records');
-                    var correctRecord = [];
-
-                    for (var i = 0; i < length; i++) {
-                        if (records[i] != undefined)
-                            correctRecord[i] = records[i];
-                        else
-                            correctRecord[i] = "";
-                    }
-                    modelsList.push(correctRecord);
+                    modelsList[row][col] = value;
                     model.set('records', modelsList);
                     return model.save();
                 });
-            }).catch((err) => {
-                console.log('Following error occurred: ', err);
-            });
-        }
+            }
     }
 });
